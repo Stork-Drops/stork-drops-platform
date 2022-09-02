fd4a87990f294c9ad1c1b42cc06ed78cb69120b7
Tyler Brangman <brangman.tyler@gmail.com>
2022-09-02 14:11:09 -0500

    new message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -146,6 +146,10 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
                                                 `,
                                                 }}
                                             />
+                                            <div className="block md:flex justify-center md:items-center md:justify-between px-5 py-2 bg-green-200 text-green-600 font-base md:text-center w-full">
+                                                <span className="text-xs font-semibold">We entered into the Solana Summer Camp Hackathon. Give us an upvote if you enjoyed the experience. 🙏</span>
+                                                <a className="flex items-center justify-center bg-green-600 text-white px-2 py-1 font-semibold mt-2.5 md:mt-0 text-xs rounded-md" target="_blank" href="https://solana.com/summercamp/voting/stork-drops">Go Vote!</a>
+                                            </div>
                                             <Component {...pageProps} />
                                         </WalletModalProvider>
                                     </ProfileProvider>
--- pages/api/getSNSAccounts.ts
+++ pages/api/getSNSAccounts.ts
@@ -1,4 +1,8 @@
+import useSWR from "swr"
+import { useWallet } from '@solana/wallet-adapter-react'
+
 export default function handler(req, res) {
-    
-    
+    const { publicKey } = useWallet();
+    const fetcher = url => fetch(url).then(r => r.json())
+    const { data } = useSWR(`https://api.helius.xyz/v0/addresses/${publicKey}/transactions?api-key=ba739f74-3869-40bb-bfd3-3cfc4be8ef7c`, fetcher);
 }
